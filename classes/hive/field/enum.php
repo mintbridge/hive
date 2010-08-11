1c22abe74f9951af71ebab5e1cbf6f8f46924b8f
Woody Gilk <woody.gilk@kohanaphp.com>
2010-08-11 02:05:02 -0500

    Added Enum field

---
+++ classes/hive/field/enum.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Hive is an intelligent modeling system for Kohana.
+ *
+ * @package    Hive
+ * @category   Field
+ * @author     Woody Gilk <woody@wingsc.com>
+ * @copyright  (c) 2008-2009 Woody Gilk
+ * @license    MIT
+ */
+class Hive_Field_Enum extends Hive_Field_String {
+
+} // End Hive_Field_Enum
