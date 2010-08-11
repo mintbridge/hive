0a090cf1910e41fcc914df07dca1a6444e83806c
Woody Gilk <woody.gilk@kohanaphp.com>
2010-08-11 15:07:31 -0500

    Added Password field type

---
+++ classes/hive/field/password.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Hive is an intelligent modeling system for Kohana.
+ *
+ * @package    Hive
+ * @category   Field
+ * @author     Woody Gilk <woody@wingsc.com>
+ * @copyright  (c) 2010 Woody Gilk
+ * @license    MIT
+ */
+class Hive_Field_Password extends Hive_Field_String {
+
+} // End Hive_Field_Password
